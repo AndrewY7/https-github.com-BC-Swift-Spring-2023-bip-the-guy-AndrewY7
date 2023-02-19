bcd6e45b2acc3843984f5562c1469010d668ccd1
Andyrewy7 <89996690+Andyrewy7@users.noreply.github.com>
2023-02-19 13:17:12 -0500

    Initial Commit

---
+++ BipTheGuy-SwiftUI/BipTheGuy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy_SwiftUIApp.swift
+//  BipTheGuy-SwiftUI
+//
+//  Created by Andrew Yang on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
